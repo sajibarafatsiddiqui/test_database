8376a84b5c3382d7d1f1fb72e4dc1b659202b8a1
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-03-30 10:12:22 +0600

    Added column. inserted data, made transactions and added queries

--- queries.sql
+++ queries.sql
@@ -9,14 +9,19 @@ select * from animals where neutered = true;
 select * from animals where name <> "Gabumon";
 select * from animals where weight_kg >= 10.4 and weight_kg <= 17.3;
 select * from animals;
+BEGIN;
 UPDATE ANIMALS SET species = 'unspecified';
 select * from animals;
 ROLLBACK;
 select * from animals;
+
+BEGIN;
 delete from animals;
 select * from animals;
 rollback;
 select * from animals;
+
+BEGIN;
 delete from animals where date_of_birth > '2022-01-01';
 savepoint one;
 update animals set weight_kg =  weight_kg*(-1);
@@ -25,6 +30,7 @@ rollback to one;
 select * from animals;
 update animals  set weight_kg = weight_kg*(-1) where weight_kg<0;
 commit;
+
 select count(*) from animals;
 select count(*) from animals where escape_attempts=0;
 select avg(weight_kg) from animals;
